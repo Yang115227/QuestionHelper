6e3ed980b35c78f98200b1a1fc7445eed8e086a4
Yang115227 <Yang115227@users.noreply.github.com>
2026-08-08 04:21:38 +0000

    修复 MainActivity 缺少导入和 AccessibilitySearchService 截图 API
    - MainActivity: 添加 QuestionRepository 导入
    - AccessibilitySearchService: 使用 ContextCompat.getMainExecutor 和反射获取 bitmap

--- app/src/main/java/com/questionhelper/MainActivity.kt
+++ app/src/main/java/com/questionhelper/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.questionhelper.bank.QuestionBankActivity
+import com.questionhelper.data.QuestionRepository
 import com.questionhelper.search.CameraSearchActivity
 import com.questionhelper.search.FloatWindowService
 import com.questionhelper.search.ScreenCaptureService
